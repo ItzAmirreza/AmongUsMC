d81dfdcf17904146010294e072469528ad0e0416
_nerodev_ <mehdisalaripsn@gmail.com>
2020-11-16 09:46:32 -0800

    added mysql functions ...

--- src/main/java/dev/ircode/amongus/database/connector.java
+++ src/main/java/dev/ircode/amongus/database/connector.java
@@ -1,8 +1,14 @@
 package dev.ircode.amongus.database;
 
 import dev.ircode.amongus.AmongUs;
+import org.bukkit.event.inventory.ClickType;
+import sun.tools.jconsole.Tab;
 
+import javax.xml.transform.Result;
+import java.sql.Array;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class connector {
     public static Connection getDatabaseConnection() {
@@ -11,5 +17,65 @@ public class connector {
         } else {
             return sqlite.getSqliteConnection();
         }
+    }
+
+    public static boolean insert(String Table_name, String[] names, String[] values, String[] types) throws SQLException {
+        String Names_sql = "(";
+        String Values_Sql = "(";
+
+        int counter = 0;
+        for (String name: names) {
+            if (counter == names.length) {
+                Names_sql = Names_sql + "`" + name + "`)";
+            } else {
+                Names_sql = Names_sql + "`" + name + "`, ";
+            }
+            counter++;
+        }
+        counter = 0;
+        for (String value: values) {
+            if (counter == values.length) {
+                if (types[counter].equalsIgnoreCase("string") || types[counter].equalsIgnoreCase("int")) {
+                    Values_Sql = Values_Sql + "'" + value + "')";
+                } else {
+                    Values_Sql = Values_Sql  + value + ")";
+                }
+            } else {
+                if (types[counter].equalsIgnoreCase("string") || types[counter].equalsIgnoreCase("int")) {
+                    Values_Sql = Values_Sql + "'" + value + "')";
+                } else {
+                    Values_Sql = Values_Sql  + value + ")";
+                }
+            }
+            counter++;
+        }
+
+        String Query = "INSERT INTO `" + Table_name + "`" + Names_sql + " VALUES " + Values_Sql;
+        if (names.length == values.length) {
+            getDatabaseConnection().createStatement().executeQuery(Query);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ResultSet update(String Table_name, String name, String value, String value_type , String Condition_name, String Condition_value) throws SQLException {
+        String Query;
+        if (value_type.equalsIgnoreCase("string") || value_type.equalsIgnoreCase("int"))  {
+            Query = "UPDATE `" + Table_name + "` SET `" + name + "`='" + value + "' WHERE " + Condition_name + "=' " + Condition_value + " '";
+        } else {
+            Query = "UPDATE `" + Table_name + "` SET `" + name + "`=" + value + " WHERE " + Condition_name + "=' " + Condition_value + " '";
+        }
+        return getDatabaseConnection().createStatement().executeQuery(Query);
+    }
+
+    public ResultSet delete(String Table_name, String Condition_name , String Condition_value) throws SQLException {
+        String Query = "DELETE FROM `" + Table_name + "` WHERE " + Condition_name + "='" + Condition_value + "'";
+        return getDatabaseConnection().createStatement().executeQuery(Query);
+    }
+
+    public ResultSet getSingleRow(String Table_name, String Condition_name, String Condition_value) throws SQLException {
+        String Query = "SELECT * FROM " + Table_name + " WHERE " + Condition_name + "='" + Condition_value + "' LIMIT 1";
+        return getDatabaseConnection().createStatement().executeQuery(Query);
     }
 }
